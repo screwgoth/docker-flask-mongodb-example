691f4eee3fb2652a3fb6b08b21210ce9bd1eb084
Raseel Bhagat <raseel.bhagat@zymr.com>
2021-06-29 12:24:50 +0530

    Add basic Authentication to the curl Request. Also added search requests

--- import.sh
+++ import.sh
@@ -14,10 +14,12 @@ curl -X POST "http://localhost:81/users/2" -H "accept: application/json" -H "Con
 curl -X POST "http://localhost:81/users/3" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "email=tor%40gmail.com&name=Tor"
 
 # fulltext search
-curl -X PUT "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=Who%20has%20many%20apples%3F"
-curl -X PUT "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=The%20apple%20tree%20grew%20in%20the%20park"
-curl -X PUT "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=Some%20apples%20are%20green%20some%20are%20yellow"
-curl -X PUT "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=How%20many%20trees%20are%20there%20in%20this%20forest%3F"
+curl -X PUT --user admin:changeme "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=Who%20has%20many%20apples%3F"
+curl -X PUT --user admin:changeme "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=The%20apple%20tree%20grew%20in%20the%20park"
+curl -X PUT --user admin:changeme "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=Some%20apples%20are%20green%20some%20are%20yellow"
+curl -X PUT --user admin:changeme "http://localhost:82/fulltext" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "expression=How%20many%20trees%20are%20there%20in%20this%20forest%3F"
+curl -X GET --user admin:changeme "http://localhost:82/search/apple" -H "accept: application/json"
+curl -X GET --user admin:changeme "http://localhost:82/search/tree" -H "accept: application/json"
 
 # geolocation-search
 curl -X POST "http://localhost:83/location" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "name=Bucharest&lat=44.2218653&lng=26.1753655"
